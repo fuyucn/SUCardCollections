80e4a3b713b50245563b3240534b64efd8a0ddbf
fuyucn <ryan.fuyu@gmail.com>
2026-07-17 16:21:44 -0700

    添加全局访问密码保护：复用上传密码验证，localStorage存储一周过期

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Guide from './components/Guide'
 import Upload from './components/Upload'
+import AuthGuard from './components/AuthGuard'
 import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/guide" element={<Guide />} />
-        <Route path="/upload" element={<Upload />} />
-      </Routes>
+      <AuthGuard>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </AuthGuard>
     </HashRouter>
   </React.StrictMode>,
 )
